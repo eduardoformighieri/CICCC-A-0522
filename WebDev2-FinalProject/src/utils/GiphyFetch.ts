8d166cec433050af5e5709748894e0272c05d9ca
Eduardo Silva <eduardolima1998@gmail.com>
2022-10-31 16:54:37 -0300

    change token

--- WebDev2-FinalProject/src/utils/GiphyFetch.ts
+++ WebDev2-FinalProject/src/utils/GiphyFetch.ts
@@ -1,3 +1,3 @@
 import { GiphyFetch } from '@giphy/js-fetch-api';
 
-export const gf = new GiphyFetch('sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh');
+export const gf = new GiphyFetch('9ghTlieo2mpsoUP0iClfCvrb1i7myeSx');
